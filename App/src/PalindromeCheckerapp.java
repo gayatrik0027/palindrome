f71bb27783ee7079b2aa7b7efb7ef12b6ecb8ca5
gayatrik0027 <gk9065@srmist.edu.in>
2026-03-06 09:00:49 +0530

    uc7

--- App/src/PalindromeCheckerapp.java
+++ App/src/PalindromeCheckerapp.java
@@ -3,34 +3,30 @@ import java.util.*;
 public class PalindromeCheckerapp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a word: ");
+
         String input = sc.nextLine();
+        Deque<Character> deque = new ArrayDeque<>();
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-
-        // Enqueue and Push characters
-        for (char ch : input.toCharArray()) {
-            queue.add(ch);     // Enqueue
-            stack.push(ch);    // Push
+        // Insert characters into deque
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue and pop
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
+        // Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if (isPalindrome) {
+        if (isPalindrome)
             System.out.println("Palindrome");
-        } else {
+        else
             System.out.println("Not a Palindrome");
-        }
 
         sc.close();
     }
